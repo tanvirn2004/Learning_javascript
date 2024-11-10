37744894a43145f7c43237670c2fdc246ffb6f92
Tanvir Rahman Nibir <tanvirrahmannibir2004@gmail.com>
2024-11-11 02:40:29 +0600

    Create 3JS.js

---
+++ 3JS.js
@@ -0,0 +1,9 @@
+const num1 = parseInt(prompt("Enter the first number:"));
+const num2 = parseInt(prompt("Enter the second number:"));
+const num3 = parseInt(prompt("Enter the third number:"));
+
+const sum = num1 + num2 + num3;
+const product = num1 * num2 * num3;
+const average = sum / 3;
+
+document.getElementById("result").innerHTML = `Sum: ${sum}<br>Product: ${product}<br>Average: ${average}`;
